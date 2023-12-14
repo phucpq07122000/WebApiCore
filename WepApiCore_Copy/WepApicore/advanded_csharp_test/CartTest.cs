c8aaf73b3f453ec37d8717adbeae843b0a510a79
PhucPQ_00 <phucpq07122000@gmail.com>
2023-12-14 21:03:02 +0700

    update

--- WepApiCore_Copy/WepApicore/advanded_csharp_test/CartTest.cs
+++ WepApiCore_Copy/WepApicore/advanded_csharp_test/CartTest.cs
@@ -1,5 +1,4 @@
-﻿using advanded_csharp_database.Models;
-using advanded_csharp_dto.Response;
+﻿using advanded_csharp_dto.Response;
 using advanded_csharp_service.CartService;
 
 namespace advanded_csharp_test
@@ -16,7 +15,7 @@ namespace advanded_csharp_test
         [TestMethod]
         public async Task getCart()
         {
-         //   string guild = "DB07579C-CED4-4D12-9883-0000E0FF4EDF";
+            //   string guild = "DB07579C-CED4-4D12-9883-0000E0FF4EDF";
             Guid id = new("23D1252A-4C8B-4786-4D7C-08DBFAC7B838");
             ListProductInCartResponse cartOfUserResponse = await _iCartService.GetCart(id);
             Assert.IsNotNull(cartOfUserResponse);
--- WepApiCore_Copy/WepApicore/advanded_csharp_test/OrderTest.cs
+++ WepApiCore_Copy/WepApicore/advanded_csharp_test/OrderTest.cs
@@ -1,15 +1,11 @@
-﻿using advanded_csharp_service.Log4net;
+﻿using advanded_csharp_dto.Response;
 using advanded_csharp_service.OrderService;
-using advanded_csharp_service;
-using advanded_csharp_database.Models;
-using advanded_csharp_dto.Response;
 
 namespace advanded_csharp_test
 {
     [TestClass]
     public class OrderTest
     {
-        
         private readonly IOrderService _iOrderService;
 
         public OrderTest()
